3fcb5fe1b6ef46b8cf4ecb3977c6a423d4cc1644
Augusto Goulart <26435937+oAGoulart@users.noreply.github.com>
2020-01-26 19:43:20 -0500

    Fix README.md typos

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
   name='markout_html',
-  version='0.1.1',
+  version='0.1.2',
   description='A tool to extract HTML contents',
   long_description=long_description,
   long_description_content_type='text/markdown',
